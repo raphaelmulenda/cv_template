eea0f2212cb2ed0909273fdbc3b84661ca84ae42
Raphael Mulenda <mulendaraphael@yahoo.fr>
2021-10-16 14:45:46 +0200

    herokusetup

--- cv_temlate/settings.py
+++ cv_temlate/settings.py
@@ -129,7 +129,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [BASE_DIR/"static"]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFile'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
